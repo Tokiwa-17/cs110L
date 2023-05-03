768d42324dc19aa062ec1e6e8f863690a0c43015
Tokiwa-17 <2872444509@qq.com>
2023-05-03 16:19:39 +0800

    part2 milestone3

--- week2/rdiff/src/main.rs
+++ week2/rdiff/src/main.rs
@@ -18,15 +18,32 @@ fn read_file_lines(filename: &String) -> Result<Vec<String>, io::Error> {
     Ok(lines)
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn lcs(seq1: &Vec<String>, seq2: &Vec<String>) -> Grid {
     // Note: Feel free to use unwrap() in this code, as long as you're basically certain it'll
     // never happen. Conceptually, unwrap() is justified here, because there's not really any error
     // condition you're watching out for (i.e. as long as your code is written correctly, nothing
     // external can go wrong that we would want to handle in higher-level functions). The unwrap()
     // calls act like having asserts in C code, i.e. as guards against programming error.
-    unimplemented!();
     // Be sure to delete the #[allow(unused)] line above
+    let m = seq1.len();
+    let n = seq2.len();
+    let mut c = Grid::new(m + 1, n + 1);
+    for i in 0..=m {
+        c.set(i, 0, 0);
+    }
+    for j in 0..=n {
+        c.set(0, j, 0);
+    }
+    for i in 0..m {
+        for j in 0..n {
+            if seq1[i].eq(&seq2[j]) {
+                c.set(i + 1, j + 1, c.get(i, j).unwrap() + 1);
+            } else {
+                c.set(i + 1, j + 1, c.get(i + 1, j).unwrap().max(c.get(i, j + 1).unwrap()));
+            }
+        }
+    }
+    c
 }
 
 #[allow(unused)] // TODO: delete this line when you implement this function
